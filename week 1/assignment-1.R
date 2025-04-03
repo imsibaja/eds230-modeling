56c0c42399e923ca69fe3d34e61f8e536f111199
ianmorris13 <97080872+ianmorris13@users.noreply.github.com>
2025-04-02 23:23:38 -0700

    assignment 1 push

---
+++ week 1/assignment-1.R
@@ -0,0 +1,17 @@
+#     $E = A * r * H * PR$
+
+# * E is energy (kWh),
+# * A is the solar panel area (m2)
+# * r is panel yield (0-1) (manufacture efficiency - usually around 0.2),
+# * PR is performance ratio (0-1) (accounting for site factors that impact efficiency usually around 0.75) 
+# * H is annual average solar radiation (kWh)
+
+photovoltaic_energy <- function(A, r=.2, PR=.75, H){
+  if(r > 1 | r < 0){
+    stop("Panel Yield (r) must be between 0 and 1 (Default = 0.2)")   
+  }
+  if(PR > 1 | PR < 0){
+    stop("Performance Ratio (r) must be between 0 and 1 (Default = 0.75)")   
+  }
+  return (A * r * H * PR)
+}
